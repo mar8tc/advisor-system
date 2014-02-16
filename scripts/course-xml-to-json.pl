be5bf4cdd8adb15fe884fc15d11ebbe149defc61
mjk522 <mjk522@mail.umsl.edu>
2014-02-16 10:43:07 -0600

    Make script to produce course list json from Courses.xml
    The script is far from perfect.  The json that it produces has an extra comma on the second to last line which must be removed in order for the json to be valid.
    
    Also, I wanted to include the course name, but having such long option text doesn't seem to work.

---
+++ scripts/course-xml-to-json.pl
@@ -0,0 +1,27 @@
+#/usr/bin/env perl
+
+use strict;
+use warnings FATAL => qw(all);
+
+use XML::XPath;
+use XML::XPath::XMLParser;
+
+my $xp      = XML::XPath->new(filename => $ARGV[0]);
+my $nodeset = $xp->find('/descriptions/course');
+
+print "{\n";
+foreach my $node ($nodeset->get_nodelist) {
+    my $subject = $node->find('subject/text()');
+    my $number  = $node->find('course_number/text()');
+    my $name    = $node->find('course_name/text()');
+
+    my $course_code = course_code($subject, $number);
+    printf qq|    "$course_code" : "$subject $number",\n|;
+}
+print "}\n";
+
+sub course_code {
+    my ($subject, $number) = @_;
+    $subject =~ s/ /_/g;
+    return "$subject$number";
+}
